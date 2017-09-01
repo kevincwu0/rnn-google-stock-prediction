2d22accc3306db9456a57868850b6f00301816b8
kevincwu0 <kevincwu0@mail.com>
2017-08-31 22:42:25 -0700

    Add rnn and data

---
+++ rnn.py
@@ -0,0 +1,11 @@
+# Recurrent Neural Network
+# Part 1 - Data preprocessing
+
+# CS 229 Stanford 
+# http://cs229.stanford.edu/proj2012/BernalFokPidaparthi-FinancialMarketTimeSeriesPredictionwithRecurrentNeural.pdf
+# train LSTM on five years of Google 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
