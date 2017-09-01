b2713fd089ac6d5d4ed4d8c8e9024bc5c6e3075b
kevincwu0 <kevincwu0@gmail.com>
2017-09-01 16:43:39 -0700

    Read in and format training set

--- rnn.py
+++ rnn.py
@@ -4,8 +4,17 @@
 # CS 229 Stanford 
 # http://cs229.stanford.edu/proj2012/BernalFokPidaparthi-FinancialMarketTimeSeriesPredictionwithRecurrentNeural.pdf
 # train LSTM on five years of Google 
+# Supervised Deep Learning
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+# Importing the training set
+training_set = pd.read_csv("Google_Stock_Price_Train.csv")
+
+# input/ouputs of recurrent neural (input != date, stock price)
+# but stock price at time t for input, and stock price t+1 for the output
+# create a set only with the "Open" Google stock price, extract that column
+# two-dimensional numpy array
+training_set = training_set.iloc[:,1:2].values
