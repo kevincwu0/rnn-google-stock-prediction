471dd63f285c78661b31e1ad56eecea7a0995b09
kevincwu0 <kevincwu0@gmail.com>
2017-09-01 17:19:37 -0700

    Add output layer

--- rnn.py
+++ rnn.py
@@ -51,4 +51,5 @@ regressor = Sequential()
 # Adding the input layer and the LSTM layer
 regressor.add(LSTM(units = 4, activation = 'sigmoid', input_shape = (None, 1)))
 
-
+# Adding the output layer
+regressor.add(Dense(units = 1))
